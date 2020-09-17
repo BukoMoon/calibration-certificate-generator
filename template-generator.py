e93565a1ef9f7589ed2734b7303ea11e35e99b1a
Louis Adams <louisadams228@gmail.com>
2020-09-17 16:52:34 +1000

    Updates: - Added additional classes to fit the other templates I need for the full application. - Cleaned up code to make it more readable
    Note:
    Some of the code still needs to be cleaned up.

--- template-generator.py
+++ template-generator.py
@@ -12,28 +12,26 @@ from os import path
 from docx.enum.table import WD_TABLE_ALIGNMENT, WD_ROW_HEIGHT_RULE
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
-
 # Example variables
-pic = 'HLP-Logo-Aus.png'
+img = 'HLP-Logo-Aus.png'
 current_date = '3/9/20'
 sn = 'HLP–PTPB005457'
 model = 'Pocket Temp Pro'
 name = 'L.Adams'
+months = '12'
 
-model_text = 'MODEL:  ' + model
-heading_text = 'CONFORMANCE CERTIFICATE'
-serial_text = 'Serial Number:  ' + sn
-line_text = 'This unit has had an operational and calibration'
-check_text = 'check on  ' + current_date
-next_text = '& meets the specifications as set out in the Manufacturers specification sheet ' + \
-            'included with the unit & meets the Australian Food Standards requirements'
-cert_text = 'This certificate is valid for 24 months from the above date.'
+# Text to be displayed in a conformance certificate
+heading_text = '\rMODEL: ' + model + '\rCONFORMANCE CERTIFICATE'
+paragraph_text = '\rSerial Number:  ' + sn + '\r\rThis unit has had an operational and calibration' + \
+                 '\rcheck on  ' + current_date + '\r& meets the specifications as set out in the Manufacturers' + \
+                 'specification sheet included with the unit & meets the Australian Food Standards requirements' + \
+                 '\rThis certificate is valid for' + months + 'months from the above date.'
 
-
+# Initialising the path for where the document will be saved
 dir_path = path.dirname(__file__)
 
 
-class MultipleConformanceCertTemplate:
+class Template(object):
 
     def __init__(self, date, serial_number, save_location):
         self.date = date
@@ -42,21 +40,64 @@ class MultipleConformanceCertTemplate:
         self.create_docx()
 
     def create_docx(self):
-        docu = docx.Document()
-        sections = docu.sections
-        for section in sections:
-            section.top_margin = Cm(1)
-            section.bottom_margin = Cm(1)
-            section.left_margin = Cm(1)
-            section.right_margin = Cm(1)
-        self.create_table(docu, rows=1, cols=1)
+        doc = docx.Document()
 
-        docu.save(self.save_location)
+        doc.save(self.save_location)
+
+    @staticmethod
+    def margin_size(doc, size):
+        sections = doc.sections
+        for section in sections:
+            section.top_margin = Cm(size)
+            section.bottom_margin = Cm(size)
+            section.left_margin = Cm(size)
+            section.right_margin = Cm(size)
+
+
+class CalibrationCertificate(Template):
+
+    def __init__(self, date, serial_number, save_location):
+        super().__init__(date, serial_number, save_location)
+
+    def create_docx(self):
+        doc = docx.Document()
+        super().margin_size(doc, 1)
+        doc.save(self.save_location)
+
+    @staticmethod
+    def docx_contents():
+        pass
+
+
+class SingleConformance(Template):
+
+    def __init__(self, date, serial_number, save_location):
+        super().__init__(date, serial_number, save_location)
+
+    def create_docx(self):
+        doc = docx.Document()
+
+        doc.save(self.save_location)
+
+    @staticmethod
+    def docx_contents(self):
+        pass
+
+
+class MultipleConformance(Template):
+
+    def __init__(self, date, serial_number, save_location):
+        super().__init__(date, serial_number, save_location)
+
+    def create_docx(self):
+        doc = docx.Document()
+        super().margin_size(doc, 1)
+        self.create_table(doc, 4, rows=1, cols=1)
+
+        doc.save(self.save_location)
 
     # TODO Create a table and format it the same as the conformance certificates
-    def create_table(self, document, rows=0, cols=0):
-        doc = document
-        number_of_tables = 4
+    def create_table(self, doc, number_of_tables, rows=0, cols=0):
         # Creating the first table
         for i in range(0, number_of_tables):
             table1 = doc.add_table(rows, cols)
@@ -68,7 +109,8 @@ class MultipleConformanceCertTemplate:
             self.table_contents(table1)
             doc.add_paragraph()
 
-    def table_contents(self, table):
+    @staticmethod
+    def table_contents(table):
         # Adding a paragraph to the table cell
         p = table.rows[0].cells[0].add_paragraph()
         p.height_rule = WD_ROW_HEIGHT_RULE.EXACTLY
@@ -81,24 +123,25 @@ class MultipleConformanceCertTemplate:
         r = p.add_run()
         x = p.add_run()
 
+        # Adding image
+        logo.add_picture(img, width=Cm(7.2898), height=Cm(1.9304))
+
         # Paragraph alignment and Font settings
         r.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
         x.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-
-        logo.add_picture(pic, width=Cm(7.2898), height=Cm(1.9304))
         r.bold = True
         r.underline = True
         r.font.name = 'Arial'
         r.font.size = Pt(12)
         x.font.name = 'Arial'
+        x.font.size = Pt(11)
         x.bold = False
         x.underline = False
 
         # Adding the text to each run
-        r.add_text('\r' + model_text + '\r' + heading_text).alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-        x.add_text('\r' + serial_text + '\r' + '\r' + line_text + '\r' + check_text + '\r' + next_text + '\r' +
-                   cert_text + '\r' + 'Signed:' + '\t' + name + '\r').alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+        r.add_text(heading_text).alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+        x.add_text(paragraph_text).alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
 
 
 if __name__ == '__main__':
-    MultipleConformanceCertTemplate(current_date, sn, (path.join(dir_path, 'generated.docx')))
+    MultipleConformance(current_date, sn, (path.join(dir_path, 'generated.docx')))
