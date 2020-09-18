264831dae912ffa532659e70c2e71f26a7b91de6
Louis Adams <louisadams228@gmail.com>
2020-09-18 19:13:51 +1000

    Updates: - Added 2 more templates, they still need more work
    Note:
    Some of the code still needs to be cleaned up.

--- template-generator.py
+++ template-generator.py
@@ -14,7 +14,7 @@ from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 
 # Example variables
 img = 'HLP-Logo-Aus.png'
-current_date = '3/9/20'
+set_date = '3/9/20'
 sn = 'HLP–PTPB005457'
 model = 'Pocket Temp Pro'
 name = 'L.Adams'
@@ -23,9 +23,24 @@ months = '12'
 # Text to be displayed in a conformance certificate
 heading_text = '\rMODEL: ' + model + '\rCONFORMANCE CERTIFICATE'
 paragraph_text = '\rSerial Number:  ' + sn + '\r\rThis unit has had an operational and calibration' + \
-                 '\rcheck on  ' + current_date + '\r& meets the specifications as set out in the Manufacturers' + \
+                 '\rcheck on  ' + set_date + '\r& meets the specifications as set out in the Manufacturers' + \
                  'specification sheet included with the unit & meets the Australian Food Standards requirements' + \
                  '\rThis certificate is valid for' + months + 'months from the above date.'
+
+# Text to be displayed for the single conformance certificate
+compliance_heading = 'Certificate of Compliance\rModel:  ' + model
+# Add valid_text in-between compliance_heading and compliance_text
+compliance_text = '\rThe above unit meets the stated specifications as set out in the Manufacturers specification ' + \
+                  'sheet included with the unit & meets the Australian Food Standards requirements.\r\r\r\r\r\r' + \
+                  'Signed\t' + name + '\t\tDate Issued: ' + set_date
+
+# Text to be displayed in a calibration certificate
+heading_text2 = '\rCalibration Certificate'
+valid_text = '(This Certificate valid for ' + months + ' Months)'
+paragraph_text2 = '\rChecked against NATA calibrated unit – AZ8801\rS/N  ' + sn + 'Calibration Report: 41598-4\r\r' +\
+                  'The above unit meets the stated specifications as set out in the Manufacturers specification ' + \
+                  'sheet included with the unit & meets the Australian Food Standards requirements.\r\r\r\r' + \
+                  'Signed\t' + name + '\t\tDate Issued: ' + set_date
 
 # Initialising the path for where the document will be saved
 dir_path = path.dirname(__file__)
@@ -61,12 +76,25 @@ class CalibrationCertificate(Template):
 
     def create_docx(self):
         doc = docx.Document()
-        super().margin_size(doc, 1)
+        super().margin_size(doc, 3)
+        self.docx_contents(doc)
         doc.save(self.save_location)
 
     @staticmethod
-    def docx_contents():
-        pass
+    def docx_contents(doc):
+        # TODO Rescale img width and height to fit page
+        doc.add_picture(img, width=Cm(17.3), height=Cm(3.95))
+        heading = doc.add_paragraph(heading_text2)
+        valid = doc.add_paragraph(valid_text)
+        text = doc.add_paragraph(paragraph_text2)
+
+        # Font Settings for each paragraph
+        heading.font.name = 'Times New Roman'
+        valid.font.name = 'Times New Roman'
+        text.font.name = 'Times New Roman'
+        heading.font.size = Pt(30)
+        heading.font.size = Pt(10)
+        heading.font.size = Pt(18)
 
 
 class SingleConformance(Template):
@@ -76,12 +104,28 @@ class SingleConformance(Template):
 
     def create_docx(self):
         doc = docx.Document()
-
+        super().margin_size(doc, 3)
+        self.docx_contents(doc)
         doc.save(self.save_location)
 
     @staticmethod
-    def docx_contents(self):
-        pass
+    def docx_contents(doc):
+        doc.add_picture(img, width=Cm(17.3), height=Cm(3.95))
+        heading = doc.add_paragraph().add_run().add_text(heading_text2)
+        valid = doc.add_paragraph().add_run().add_text(valid_text)
+        text = doc.add_paragraph().add_run().add_text(paragraph_text2)
+
+        heading_run = heading.run
+        valid_run = valid.run
+        text_run = text.run
+
+        # Font Settings for each paragraph
+        heading_run.font.name = 'Times New Roman'
+        valid_run.font.name = 'Times New Roman'
+        text_run.font.name = 'Times New Roman'
+        heading_run.font.size = Pt(30)
+        valid_run.font.size = Pt(10)
+        text_run.font.size = Pt(18)
 
 
 class MultipleConformance(Template):
@@ -100,13 +144,13 @@ class MultipleConformance(Template):
     def create_table(self, doc, number_of_tables, rows=0, cols=0):
         # Creating the first table
         for i in range(0, number_of_tables):
-            table1 = doc.add_table(rows, cols)
-            table1.cell(0, 0).width = Cm(9)
-            table1.alignment = WD_TABLE_ALIGNMENT.LEFT
-            table1.style = 'TableGrid'
-            table1.autofit = False
-            table1.width = Cm(9)
-            self.table_contents(table1)
+            table = doc.add_table(rows, cols)
+            table.cell(0, 0).width = Cm(9)
+            table.alignment = WD_TABLE_ALIGNMENT.LEFT
+            table.style = 'TableGrid'
+            table.autofit = False
+            table.width = Cm(9)
+            self.table_contents(table)
             doc.add_paragraph()
 
     @staticmethod
@@ -144,4 +188,7 @@ class MultipleConformance(Template):
 
 
 if __name__ == '__main__':
-    MultipleConformance(current_date, sn, (path.join(dir_path, 'generated.docx')))
+    # Generated Calibration Certificate must have 'device_model and serial number' for the name.
+    MultipleConformance(set_date, sn, (path.join(dir_path, 'generated.docx')))
+    CalibrationCertificate(set_date, sn, (path.join(dir_path, 'generated-calibration.docx')))
+    SingleConformance(set_date, sn, (path.join(dir_path, 'generated-conformance-single.docx')))
